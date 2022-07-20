83154c58aa0bf335b8aec45761bf2e0d6cf58faa
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2022-07-20 20:49:40 +0800

    Basic telegram api example

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-const hello = (name: string) => {
-    console.log(`HELLO ${name}`)
-}
+import path from 'path'
+import MTProto from '@mtproto/core' 
+import { telegramConfig } from './config'
 
-hello("WORLD!")
+const mtproto = new MTProto({
+    api_id: telegramConfig.apiId,
+    api_hash: telegramConfig.apiHash,
+    storageOptions: {
+        path: path.resolve(__dirname, './data/1.json'),
+    },
+    test: false
+})
+
+mtproto.call('help.getNearestDc')
+    .then((result: any) => {
+        console.log('country: ', result.country)
+    })
+    .catch((err: any) => {
+        console.log(err)
+    })
